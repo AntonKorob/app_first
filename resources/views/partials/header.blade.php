c0e91354b8c6bf42292c3f712d44567c242204b7
anton_git <gorizont.k@mail.ru>
2024-06-30 23:18:54 +0700

    Обновил регистрацию и авторизацию

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,10 +2,20 @@
     class="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
     <div class="mb-2 sm:mb-0 inner">
 
-        <a href="{{ route("home") }}"
+        <a href="{{ route('home' ) }}"
             class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">Laravel с
             нуля</a><br>
         <span class="text-xs text-grey-dark">Мои первые шаги в Laravel</span>
+
+        <div class="sm:mb-0 self-center">
+            @auth("web")
+            <a href="{{ route( 'logout' )}}" class=" btn btn-info text-decoration-none ">Выйти</a>
+            @endauth
+
+            @guest("web")
+            <a href="{{ route( 'login' )}}" class=" btn btn-info text-decoration-none ">Войти</a>
+            @endguest
+        </div>
 
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
 use App\Http\Middleware\GoogleRecaptcha;
 use App\Http\Middleware\EnsureTokenlsValid;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +22,29 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [IndexController::class, 'index'])->name('home');
 
+// Вывод статей из DB на странице
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+
+
+Route::middleware("auth:web")->group(function(){
+    
+    // Вывод logout на странице
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    
+});
+
+Route::middleware("guest:web")->group(function(){
+    
+    // Вывод login на странице
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    
+    Route::post('/login_process', [AuthController::class, 'login'])->name('login_process');
+    
+    // Вывод registerForm на странице
+    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+    
+    // Вывод Post meth на странице
+    Route::post('/register_process', [AuthController::class, 'register'])->name('register_process');
+});
